d9578811b2ed5e2ee44aba24514cbe800b9e5278
SudiDav <sudisimbadav@gmail.com>
2022-08-14 23:09:18 +0200

    chore: added order controller and Get Orders endpoint

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+using System.Formats.Asn1;
+using API.Data;
+using API.Entities.OrderAggregate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class OrdersController : BaseApiController
+    {
+        private readonly ILogger<OrdersController> _logger;
+        private readonly StoreContext _context;
+
+        public OrdersController(ILogger<OrdersController> logger, StoreContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<Order>>> GetOrders()
+        {
+            return await _context.Orders
+                    .Include(o => o.OrderItems)
+                    .Where(order => order.BuyerId == User.Identity.Name)
+                    .ToListAsync();
+        }
+    }
+}
