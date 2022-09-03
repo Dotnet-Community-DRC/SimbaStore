b85b20fc371d3f06c66674fbf62254d2ed197ef3
SudiDav <sudisimbadav@gmail.com>
2022-09-03 08:57:09 +0200

    added stripe validation on client

--- client/src/features/checkout/CheckoutWrapper.tsx
+++ client/src/features/checkout/CheckoutWrapper.tsx
@@ -1,12 +1,29 @@
-import {Elements} from "@stripe/react-stripe-js";
-import {loadStripe} from "@stripe/stripe-js";
-import CheckoutPage from "./CheckoutPage";
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { useEffect, useState } from 'react';
+import agent from '../../app/api/agent';
+import LoadingComponent from '../../app/layout/LoadingComponent';
+import { useAppDispatch } from '../../app/store/configureStore';
+import { setBasket } from '../basket/basketSlice';
+import CheckoutPage from './CheckoutPage';
 
 const stripePromise = loadStripe(
-  "pk_test_51La4HADZC98dTivt6hch36yFmynDCqVM4kVsl78LAJelGnJS6TYpIWqiKNUCSDSvJgyDrGl21RYDkTPMrYrUqlHd00mbumF7Ct"
+  'pk_test_51La4HADZC98dTivt6hch36yFmynDCqVM4kVsl78LAJelGnJS6TYpIWqiKNUCSDSvJgyDrGl21RYDkTPMrYrUqlHd00mbumF7Ct'
 );
 
 export default function CheckoutWrapper() {
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    agent.Payments.createPaymentIntent()
+      .then(basket => dispatch(setBasket(basket)))
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false));
+  });
+
+  if (loading) return <LoadingComponent message='Loading checkout...' />;
+
   return (
     <Elements stripe={stripePromise}>
       <CheckoutPage />
