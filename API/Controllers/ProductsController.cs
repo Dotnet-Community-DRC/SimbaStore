f4e62f37d479d1dee92a69e946818d7e9e0ef6c2
SudiDav <sudisimbadav@gmail.com>
2022-09-24 14:23:14 +0200

    Completed the CRUD backend functionalities

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -86,7 +86,7 @@ namespace API.Controllers
 
         [Authorize(Roles="Admin")]
         [HttpPut]
-        public async Task<ActionResult> UpdateProduct(UpdateProductDto productDto)
+        public async Task<ActionResult<Product>> UpdateProduct([FromForm]UpdateProductDto productDto)
         {
             var product = await _context.Products.FindAsync(productDto.Id);
 
@@ -94,9 +94,25 @@ namespace API.Controllers
 
             _mapper.Map(productDto, product);
 
+            if (productDto.File is not null)
+            {
+                var imageResult = await _imageService.AddImageAsync(productDto.File);
+
+                if(imageResult.Error is not null)
+                    return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
+
+                    //Delete an image if it is already exit
+
+                if(!string.IsNullOrEmpty(product.PublicId))
+                    await _imageService.DeleteImageAsync(product.PublicId);
+
+                product.PictureUrl = imageResult.SecureUrl.ToString();
+                product.PublicId = imageResult.PublicId;
+            }
+
             var result = await _context.SaveChangesAsync() > 0;
 
-            if(result) return NoContent();
+            if(result) return Ok(product);
 
             return BadRequest(new ProblemDetails{Title = "Problem updating product!"});
         }
@@ -108,6 +124,9 @@ namespace API.Controllers
             var product = await _context.Products.FindAsync(id);
 
             if(product is null) return NotFound();
+
+            if(!string.IsNullOrEmpty(product.PublicId))
+                    await _imageService.DeleteImageAsync(product.PublicId);
 
             _context.Products.Remove(product);
 
