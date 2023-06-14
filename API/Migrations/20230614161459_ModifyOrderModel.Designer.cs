eb57eae846ecfce3834ef54d2b713dbe88145c1c
SudiDav <sudisimbadav@gmail.com>
2023-06-14 09:25:03 -0700

    modify order model and add new migration

--- API/Migrations/20230614161459_ModifyOrderModel.Designer.cs
+++ API/Migrations/20230614161459_ModifyOrderModel.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace API.Data.Migrations
+namespace API.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20230614161459_ModifyOrderModel")]
+    partial class ModifyOrderModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
